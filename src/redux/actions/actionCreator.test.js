581b3752903251d9488fbf59973aa27d8220cd7b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-14 00:45:55 +0100

    Test for userLogoutAction action creator added

--- src/redux/actions/actionCreator.test.js
+++ src/redux/actions/actionCreator.test.js
@@ -1,18 +1,31 @@
 import { getUser } from "../../factories/usersFactory";
-import { userLoginAction } from "./actionCreator";
+import { userLoginAction, userLogoutAction } from "./actionCreator";
 import actionTypes from "./actionTypes";
 
 describe("Given a userLoginAction action creator", () => {
   describe("When it receives a user", () => {
     test("Then it should return a userLogin action with a user received", () => {
       const user = getUser();
-      console.log("user: ", user);
       const expectedAction = {
         type: actionTypes.userLogin,
         user,
       };
 
       const actionResult = userLoginAction(user);
+
+      expect(actionResult).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a userLogoutAction action creator", () => {
+  describe("When it invokes", () => {
+    test("Then it should return a userLogout action", () => {
+      const expectedAction = {
+        type: actionTypes.userLogout,
+      };
+
+      const actionResult = userLogoutAction();
 
       expect(actionResult).toEqual(expectedAction);
     });
