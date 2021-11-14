f250e823b70e06784c8d8d59541286d5cad31a7a
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-14 01:27:00 +0100

    Thunk loginUserThunk added

---
+++ src/redux/thunks/userThunks.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { userLoginAction } from "../actions/actionCreator";
+
+const urlApi = process.env.REACT_APP_API_URL;
+const localStorageKey = process.env.REACT_APP_LOCALSTORAGE_KEY;
+
+export const loginUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(urlApi + "/users/login", user);
+  if (response.status === 200) {
+    const token = response.data.token;
+    const user = jwtDecode(token);
+    dispatch(userLoginAction(user));
+    localStorage.setItem(localStorageKey, JSON.stringify({ token: token }));
+  } else {
+    const error = new Error("Thunk error!");
+    error.code = 400;
+  }
+};
