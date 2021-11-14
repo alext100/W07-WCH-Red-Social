1e51eb9534c5b9d78b066311a6c07a7c6c5d00cc
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-14 15:03:28 +0100

    Routes to App.js added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
+import Header from "./components/Header";
+import LoginForm from "./components/loginForm/LoginForm";
+import RegisterForm from "./components/registerForm/RegisterForm";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
