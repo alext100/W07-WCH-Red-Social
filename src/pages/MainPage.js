29f456a0c97de815d07608eadc3d3f281f146641
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-14 15:02:41 +0100

    Template of the MainPage created

---
+++ src/pages/MainPage.js
@@ -0,0 +1,10 @@
+const MainPage = () => {
+  return (
+    <p>
+      Main Page (I'm going to add a list of all users here, the option to choose
+      friends and enemies)
+    </p>
+  );
+};
+
+export default MainPage;
