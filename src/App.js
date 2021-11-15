5ff89f8beb7dbe02994a11d54def17c9078b14e4
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-15 11:42:30 +0100

    To App.js path=/* was added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import LoginForm from "./components/loginForm/LoginForm";
 import RegisterForm from "./components/registerForm/RegisterForm";
+import useUsers from "./hooks/useUsers";
 import MainPage from "./pages/MainPage";
 
 function App() {
+  const { isUserLoggedIn } = useUsers();
+
+  useEffect(() => {
+    isUserLoggedIn();
+  }, [isUserLoggedIn]);
+
   return (
     <>
       <BrowserRouter>
@@ -15,6 +23,7 @@ function App() {
           <Route path="/main" element={<MainPage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/*" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
     </>
