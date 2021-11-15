ebcc3681942bf68673e82064811f751c434be3f8
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-15 19:29:57 +0100

    MainPage Route was wrapped into PrivatRoute

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import useUsers from "../../hooks/useUsers";
 import LoginForm from "../loginForm/LoginForm";
 
 const PrivateRoute = ({ children }) => {
-  const user = useUsers();
+  const { isUserLoggedIn, user } = useUsers();
+
+  useEffect(() => {
+    isUserLoggedIn();
+  }, [isUserLoggedIn]);
+
+  console.log("user.isAuthenticated: ", user.isAuthenticated);
   return user.isAuthenticated ? children : <LoginForm />;
 };
 
