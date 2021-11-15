659e938325d28c8f4ad95677b1b57bbdfe7dc087
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-15 18:10:18 +0100

    To MainPage added usersList.map to show users cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import "./Header.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { isAuthenticated } = useSelector(({ user }) => user);
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
       <Container>
@@ -10,15 +13,20 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className="header-navlink" to="/main">
-              Home
-            </NavLink>
-            <NavLink className="header-navlink" to="/login">
-              Login
-            </NavLink>
-            <NavLink className="header-navlink" to="/register">
-              Register
-            </NavLink>
+            {isAuthenticated ? (
+              <NavLink className="header-navlink" to="/main">
+                Main
+              </NavLink>
+            ) : (
+              <>
+                <NavLink className="header-navlink" to="/login">
+                  Login
+                </NavLink>
+                <NavLink className="header-navlink" to="/register">
+                  Register
+                </NavLink>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
